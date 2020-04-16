fc1fbbabca14e598c9b79cb2f6a9acba4b609d7c
吴彦文 <jaguarjack@wuyanwendeMacBook-Pro.local>
2020-04-16 10:21:59 +0800

    新增组件map

--- src/config/componentsMap.js
+++ src/config/componentsMap.js
@@ -2,11 +2,19 @@
  * export component map
  */
 export default {
+  userLayout: () => import('@/layouts/UserLayout'),
+  basicLayout: () => import('@/layouts/BasicLayout'),
+  routeView: () => import('@/layouts/RouteView'),
+  blankLayout: () => import('@/layouts/BlankLayout'),
+  pageView: () => import('@/layouts/PageView'),
   workplace: () => import('@/views/dashboard/Workplace'),
   users: () => import('@/views/permissions/users/users'),
   roles: () => import('@/views/permissions/roles/roles'),
   rules: () => import('@/views/permissions/rules/rules'),
   database: () => import('@/views/system/database/index'),
   loginLog: () => import('@/views/system/log/login'),
-  operateLog: () => import('@/views/system/log/operate')
+  operateLog: () => import('@/views/system/log/operate'),
+  attachment: () => import('@/views/system/attachments/index'),
+  departments: () => import('@/views/permissions/departments/departments'),
+  jobs: () => import('@/views/permissions/jobs/jobs'),
 }
