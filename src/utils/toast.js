44ab847633c858a7158568af3091c52fd3606ba4
JaguarJack <njphper@gmail.com>
2020-05-06 18:18:00 +0800

    修改提示

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,9 +1,9 @@
 import { message } from 'ant-design-vue'
 
-export function toast (res) {
+export function toast (res, duration = 2) {
     if (res.code === 10000) {
-      message.success(res.message)
+      message.success(res.message, duration)
     } else {
-      message.error(res.message)
+      message.error(res.message, duration)
     }
 }
