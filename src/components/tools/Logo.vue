1d0a922ae5f40b474e440f7beb57f54f239558f1
JaguarJack <njphper@gmail.com>
2020-05-18 00:17:08 +0800

    update componetns

--- src/components/tools/Logo.vue
+++ src/components/tools/Logo.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="logo">
     <router-link :to="{name:'dashboard'}">
-      <LogoSvg alt="logo" />
+      <img src="@/assets/logo.png">
       <h1 v-if="showTitle">{{ title }}</h1>
     </router-link>
   </div>
 </template>
 
 <script>
-import LogoSvg from '@/assets/logo.svg?inline'
 
 export default {
   name: 'Logo',
-  components: {
-    LogoSvg
-  },
   props: {
     title: {
       type: String,
