7e154dccff5cd2cc75fbc5a805e81ae876a5a22a
JaguarJack <njphper@gmail.com>
2020-05-20 12:57:21 +0800

    修改date函数

--- src/utils/date.js
+++ src/utils/date.js
@@ -5,12 +5,12 @@
  * @returns {string}
  */
 export function timestampToDate (timestamp) {
-    const date = new Date(timestamp)
+  const date = new Date(timestamp)
 
-    return date.getFullYear() + '-'
-          + date.getMonth() + '-'
-          + date.getDay() + ' '
-          + date.getHours() + '时'
-          + date.getMinutes() + '分'
-          + date.getDay() + '秒'
+  return date.getFullYear() + '-'
+    + (date.getMonth() + 1) + '-'
+    + date.getDate() + ' '
+    + date.getHours() + '时'
+    + date.getMinutes() + '分'
+    + date.getSeconds() + '秒'
 }
