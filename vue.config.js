2f346b1e8da0c96f38a85b69082f704858d65235
yanwenwu <njphper@gmail.com>
2020-09-11 10:33:46 +0800

    delete:delete mock service config

--- vue.config.js
+++ vue.config.js
@@ -35,8 +35,8 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
-    },
-    before: require('./mock/mock-server.js')
+    }
+    // before: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
