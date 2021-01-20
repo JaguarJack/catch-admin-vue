e82243dfd7abdab388d9bcf35a8c63cc9aa4b81c
yanwenwu <njphper@gmail.com>
2021-01-20 20:12:24 +0800

    fixed

--- src/views/system/config/component/upload.vue
+++ src/views/system/config/component/upload.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container">
     <el-form ref="form" :model="form">
-      <el-divider content-position="left"><h3>阿里 OSS 配置</h3></el-divider>
+      <el-divider content-position="left"><h3>阿里 OSS 配置 (<a href="https://help.aliyun.com/product/31815.html" target="_blank">请认真阅读 OSS 相关配置</a>)</h3></el-divider>
       <el-form-item label="prefix" :label-width="width" style="width: 600px">
         <el-input v-model="form.oss.prefix" placeholder="prefix" autocomplete="off" clearable />
       </el-form-item>
