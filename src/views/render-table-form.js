ce8b2f38bd76c11909d07928726fbf1ac0827b5c
yanwenwu <njphper@gmail.com>
2021-04-05 22:09:07 +0800

    新增mixins，减少重复代码

---
+++ src/views/render-table-form.js
@@ -0,0 +1,40 @@
+export default {
+  data() {
+    return {
+      table: {
+        tree: {},
+        dialog: {}
+      },
+      formCreate: {
+        fApi: {},
+        rule: []
+      },
+      tableFrom: ''
+    }
+  },
+   created() {
+     this.$http.get(this.tableFrom).then(response => {
+      this.table = response.data.table;
+      this.formCreate.rule = response.data.form
+
+       // 创建后的一些操作
+       if (this.afterMixinsCreated) {
+         this.afterMixinsCreated()
+       }
+    })
+  },
+
+  // 这里使用 watch 监听 table 数据变化，重新获取列表数据
+  watch: {
+    table: function() {
+      this.$nextTick(function(){
+        // 需要强制更新 table 组件
+        if (this.table.forceUpdate) {
+          this.$refs[this.table.ref].updateKey++
+        }
+
+        this.$refs[this.table.ref].getList()
+      })
+    }
+  }
+}
