3a8a269f05f3b656940339fc7fa116544340422d
yanwenwu <njphper@gmail.com>
2021-04-05 12:25:23 +0800

    修改刷新功能

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,17 @@ export const constantRoutes = [
     ]
   },
   {
+    path: '/redirect',
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: '/redirect/:path(.*)',
+        component: () => import('@/views/redirect')
+      }
+    ]
+  },
+  {
     path: '/login',
     component: () => import('@/views/login/index'),
     hidden: true
