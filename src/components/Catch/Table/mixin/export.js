0437125bc6d9d2b5c8c198b8d458d10d2d58728b
yanwenwu <njphper@gmail.com>
2021-04-24 20:27:33 +0800

    新增导入和导出组件

--- src/components/Catch/Table/mixin/export.js
+++ src/components/Catch/Table/mixin/export.js
@@ -1,23 +1,30 @@
 export default {
   methods: {
     handleExport() {
-      if (this.getParent.beforeExport !== undefined) {
+      // 使用默认的导出方式
+      if (this.getParent.table.usedModel !== undefined) {
+         this.$http.post('excel/export', {fields: JSON.stringify(this.getExportFields()), model: this.getParent.table.usedModel})
+           .then(response => {
+           window.open(response.data.url)
+         })
+      } else {
+        if (this.getParent.beforeExport !== undefined) {
           this.getParent.beforeExport()
-      }
+        }
 
+        let params = {}
+        const filterParams = this.getParent.table.filterParams
 
-      let params = {}
-      const filterParams = this.getParent.table.filterParams
-
-      Object.keys(filterParams).forEach(function(k){
+        Object.keys(filterParams).forEach(function(k) {
           if (k !== 'limit' && k !== 'page') {
             params[k] = filterParams[k]
           }
-      })
+        })
 
-      this.$http.get(this.getParent.table.exportRoute,  this.filterParams).then(response => {
-            window.open(response.data.url)
-      })
+        this.$http.get(this.getParent.table.exportRoute, this.filterParams).then(response => {
+          window.open(response.data.url)
+        })
+      }
     }
   }
 }
--- src/components/Catch/Table/mixin/import.js
+++ src/components/Catch/Table/mixin/import.js
@@ -1,11 +1,49 @@
+import { mapGetters } from 'vuex'
+
 export default {
+  computed: {
+    ...mapGetters([
+      'token',
+    ])
+  },
+  data() {
+    return {
+      importVisible: false,
+      importAction: '',
+      importHeaders: {},
+      importList: [],
+      extraImport: {},
+      importLoading: false,
+    }
+  },
   methods: {
+    handleSubmitImport() {
+      this.importLoading = true;
+      this.$refs.importUpload.submit();
+    },
     handleImport() {
+      this.importHeaders.authorization = 'Bearer ' + this.token
+
+      this.importVisible = true
+
+      this.extraImport.fields = JSON.stringify(this.getImportFields())
+      this.extraImport.model = this.getParent.table.usedModel
+
       if (this.getParent.beforeImport !== undefined) {
         this.getParent.beforeImport()
       }
 
-      alert('功能未开发')
+      // 切换 action
+      if (this.getParent.table.usedModel !== undefined) {
+         this.importAction = process.env.VUE_APP_BASE_API + '/excel/import';
+      } else {
+        this.importAction = process.env.VUE_APP_BASE_API +  this.getParent.table.importRoute;
+      }
+    },
+    importSuccess(response, file, fileList) {
+      console.log(123)
+        this.importLoading = false;
+
     }
   }
 }
