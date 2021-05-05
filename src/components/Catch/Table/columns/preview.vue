a44411b3f90cdf928ef5b1b09cc48ad14c217a9d
yanwenwu <njphper@gmail.com>
2021-05-05 16:51:56 +0800

    更新预览组件

--- src/components/Catch/Table/columns/preview.vue
+++ src/components/Catch/Table/columns/preview.vue
@@ -1,13 +1,10 @@
 <template>
-  <div v-if="$attrs.row[field].length > 0">
     <el-image
-      v-if="isImage($attrs.row[field])"
+      v-if="$attrs.row[field].length > 0"
       style="width: 50px;"
       :src="$attrs.row[field]"
       :preview-src-list="[$attrs.row[field]]">
     </el-image>
-    <el-tag v-else class="el-icon-download" @click="download($attrs.row[field])">下载</el-tag>
-  </div>
 </template>
 
 <script>
@@ -23,16 +20,6 @@ export default {
       default() {
         return 'url';
       }
-    }
-  },
-  methods: {
-    isImage(url) {
-      const ext = url.substr(url.lastIndexOf('.') + 1)
-
-      return ['png', 'jpg', 'jpeg', 'gif', 'svg', 'bmp'].indexOf(ext.toLowerCase()) !== -1
-    },
-    download(url) {
-      location.href = url
     }
   }
 }
