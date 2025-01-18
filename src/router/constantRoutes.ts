0843a61de53f760b09ddefeb1d183548aa7429f9
Joker <82664165@qq.com>
2025-01-18 09:17:11 +0800

    update component load

--- src/router/constantRoutes.ts
+++ src/router/constantRoutes.ts
@@ -1,21 +1,3 @@
-import { RouteRecordRaw } from 'vue-router'
-import { isUndefined } from '@/support/helper'
-// @ts-ignore
-export function getModuleRoutes() {
-  const modules = import.meta.glob('@/module/**/views/router.ts', { eager: true })
-  let moduleRoutes: RouteRecordRaw[] = []
-
-  Object.keys(modules).forEach(routePath => {
-    // @ts-ignore
-    if (!isUndefined(modules[routePath].default)) {
-      // @ts-ignore
-      moduleRoutes = moduleRoutes.concat(modules[routePath].default)
-    }
-  })
-
-  return moduleRoutes
-}
-
 // 获取初用户和开发模块的 view component
 export function getModuleViewComponents() {
   const components = new Map()
